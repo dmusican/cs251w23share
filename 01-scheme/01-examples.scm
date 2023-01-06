e4d2804dfbb8151c0169ab2de8a017254570fb44
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-06 12:47:02 -0600

    Scripted auto-commit on change (2023-01-06 12:47:02) by gitwatch.sh

--- 01-scheme/01-examples.scm
+++ 01-scheme/01-examples.scm
@@ -2,7 +2,12 @@
 ;; (lambda (parameters)
 ;;     function body))
 
-(lambda (x y)
-  (+ x y)
+;;(lambda (x y)
+;;  (+ x y)
 	
-(   (lambda (x y) (+ x y))  7  9)
+;;(   (lambda (x y) (+ x y))  7  9)
+
+(define my-plus
+  (lambda (x y)
+    (+ x y)))
+
