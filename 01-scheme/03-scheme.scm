e81d0ffe63f3ea09d2a836266b6e96a3189c38ab
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-09 11:56:01 -0600

    Scripted auto-commit on change (2023-01-09 11:56:01) by gitwatch.sh

--- 01-scheme/03-scheme.scm
+++ 01-scheme/03-scheme.scm
@@ -3,7 +3,8 @@
 ;; this example I'm actually producing a list
 (define remove-first
   (lambda (item lst)
-    (cond (   (null? lst)   '()   )
-	  (   (equal? item (car lst))   (cdr lst)   )
-	  (   else (cons (car lst)
-			 (remove-first item (cdr lst)))))))
+    (cond ((null? lst) '())
+	  ((equal? item (car lst)) (cdr lst))
+	  (else
+	   (cons (car lst)
+		 (remove-first item (cdr lst)))))))
