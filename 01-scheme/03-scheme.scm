918d1b151d2260ee4a733c064ae04a1ae60888d8
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-09 11:54:00 -0600

    Scripted auto-commit on change (2023-01-09 11:54:00) by gitwatch.sh

---
+++ 01-scheme/03-scheme.scm
@@ -0,0 +1,9 @@
+;;; (remove-first 'a '(b a c d)) ---> (b c d)
+
+;; this example I'm actually producing a list
+(define remove-first
+  (lambda (item lst)
+    (cond (   (null? lst)   '()   )
+	  (   (equal? item (car lst))   (cdr lst)   )
+	  (   else (cons (car lst)
+			 (remove-first item (cdr lst)))))))
