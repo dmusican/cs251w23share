29aa49842ecc99e8ed996076b0aec45ee2e59793
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-13 12:42:38 -0600

    Scripted auto-commit on change (2023-01-13 12:42:38) by gitwatch.sh

--- 01-scheme/07-higher.scm
+++ 01-scheme/07-higher.scm
@@ -5,4 +5,7 @@
 ;; map takes two parameters
 ;; the second should be a list
 ;; the first should be a function of one parameter
+;;
+;; returns a new list that runs the function
+;; on each item in that list
 ;; (map add-one '(8 2 1 9 3))
