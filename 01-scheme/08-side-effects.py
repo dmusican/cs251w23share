5a7b233e2ea0b14bed85cb099b2a50c86d8146f7
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-16 12:12:06 -0600

    Scripted auto-commit on change (2023-01-16 12:12:06) by gitwatch.sh

--- 01-scheme/08-side-effects.py
+++ 01-scheme/08-side-effects.py
@@ -3,3 +3,17 @@ def remove_last(lst):
     result = lst[:-1]
     return result
 
+# These do have side effects
+
+count = 0
+def remove_first_directly(lst):
+    global count
+    count = count + 1   # side effect
+    lst.pop()           # side effect
+    print("Working")    # side effect
+    return None
+
+thing = [9,8,7,6]
+
+remove_first_directly(thing)
+print(thing)
