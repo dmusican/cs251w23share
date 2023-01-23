b972ca887dd32fffa67d0cec9dd674120e21db26
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-23 11:43:12 -0600

    Scripted auto-commit on change (2023-01-23 11:43:12) by gitwatch.sh

--- 04-linkedlist/linkedlist.c
+++ 04-linkedlist/linkedlist.c
@@ -1,4 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 struct LinkedList {
     int value;
     struct LinkedList *next;
-}
+};
+
+typedef    struct LinkedList    LinkedList;
+
+int main() {
+    LinkedList *list = NULL;
