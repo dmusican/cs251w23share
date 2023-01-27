7a0745a0ae4279a21c23d3b35058cee541b1b927
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-27 12:37:12 -0600

    Scripted auto-commit on change (2023-01-27 12:37:12) by gitwatch.sh

---
+++ 04-linkedlist/application.c
@@ -0,0 +1,8 @@
+int main() {
+    LinkedList *list = NULL;
+    list = insertFrontLL(list, 2);
+    list = insertFrontLL(list, 5);
+    list = insertFrontLL(list, 3);
+    displayLLAgain(list);
+    cleanupLL(list);
+}
