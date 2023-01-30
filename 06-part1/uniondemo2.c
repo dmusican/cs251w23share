63fa60a2ae7a6bf66a6b3d2260a8bb0ed1523d7f
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-01-30 11:40:53 -0600

    Scripted auto-commit on change (2023-01-30 11:40:53) by gitwatch.sh

--- 06-part1/uniondemo2.c
+++ 06-part1/uniondemo2.c
@@ -1,1 +1,11 @@
 #include "value.h"
+
+int main() {
+    Value v;
+    v.i = 3;
+    v.type = INT_TYPE;
+
+    // .... much later...
+    if (v.type == INT_TYPE) {
+        printf("%i\n", v.i);
+    }
