50fc0baa6bc02db287b6a786432b1b4927118fbd
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-02-01 11:47:11 -0600

    Scripted auto-commit on change (2023-02-01 11:47:11) by gitwatch.sh

--- 07-translation/greet-interpreter.py
+++ 07-translation/greet-interpreter.py
@@ -1,11 +1,10 @@
 import sys
 inpfile = open('myprog.greet', 'r')
-outfile = open('myprog.bash', 'w')
 
 for line in inpfile:
     (command, number) = line.split()
     if int(number) < 1:
-        print('Compiler error, number too small')
+        print('Interpreter error, number too small')
         sys.exit()
     outfile.write('for i in {1..' + number + '}\n')
     outfile.write('do\n')
