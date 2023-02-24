84707a3bf4efc98e31f2c1b46ac260350c2379bd
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-02-24 12:32:51 -0600

    Scripted auto-commit on change (2023-02-24 12:32:51) by gitwatch.sh

--- ExampleLambda.java
+++ ExampleLambda.java
@@ -4,6 +4,8 @@
 
 class ExampleLambda {
 
+    public .....   evaluate( f, int x, int y) {
+        System.out.println(f(x,y));
 
     public static void main(String[] args) {
         System.out.println("hello");
