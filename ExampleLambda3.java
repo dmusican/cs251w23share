a1bd3f18e7408bab399ce457a05c8134b70cf36b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-02-24 12:52:08 -0600

    Scripted auto-commit on change (2023-02-24 12:52:08) by gitwatch.sh

---
+++ ExampleLambda3.java
@@ -0,0 +1,27 @@
+// (define evaluate
+//     (lambda (f x y)
+       // (f x y)))
+
+interface DaveOperation {
+    public int f(int a, int b);
+}
+
+class ExampleLambda2 {
+
+    public static void evaluate(
+            DaveOperation dobj, int x, int y) {
+        System.out.println(dobj.f(x,y));
+    }
+
+    public static void main(String[] args) {
+        evaluate(new DaveOperation() {
+                public int f(int a, int b) {
+                    return a+b;
+                }}, 3, 5);
+        evaluate(new DaveOperation() {
+                public int f(int a, int b) {
+                    return a-b;
+                }}, 3, 5);
+    }
+
+}
