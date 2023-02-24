0423f513ccaca401827c12ccf823e2f5f612eddc
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2023-02-24 12:22:52 -0600

    Scripted auto-commit on change (2023-02-24 12:22:52) by gitwatch.sh

--- example-lambda.py
+++ example-lambda.py
@@ -22,3 +22,9 @@ people = [("Dave", 3), ("Liz", 8),
           ("Rae", 1), ("Evan", 12)]
 people.sort(key=lambda person: person[1])
 print(people)
+
+def get_age(person):
+    return person[1]
+
+people.sort(key=get_age)
+print(people)
